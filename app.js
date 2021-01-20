727fbcf3c775303eda5b03bbc4b052e2df9006be
Rajan Maghera <rmaghera@ualberta.ca>
2021-01-20 04:18:24 -0700

    Tried to get auth working and returning a request

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import dotenv from 'dotenv'
-dotenv.config();
+dotenv.config()
 
 import express from 'express'
 import bodyParser from 'body-parser'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
+
 const app = express()
 const port = process.env.PORT
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+
 
 const baseurl = '/api'
 
@@ -19,9 +24,24 @@ var db = mongoose.connect('mongodb://localhost:27017/workhorse', {useNewUrlParse
 
 import User from './user.js'
 import Employee from './employee.js'
+import pkg from 'bson';
+const { ObjectId } = pkg;
 
 // Databsa
 
+
+// ---AUTH METHODS---
+
+function authVerify (token, next) {
+
+}
+
+function authNewRefreshToken() {
+    let refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+        algorithm: "HS256",
+        expiresIn: process.env.REFRESH_TOKEN_LIFE
+    })
+}
 
 // ---SERVER REST REQUESTS---
 
@@ -35,22 +55,31 @@ app.post(`${baseurl}/saveUser`, (req,res) => {
 
 })
 
+app.get(`${baseurl}/getUserInfo`, (req,res) => {
+
+    User.getInformation(req.cookies['jwt'], function(err,obj) {
+        if (err) res.send(err)
+
+        console.log(obj)
+
+        res.send(User.findOne({ _id: ObjectId('6007cf3994fd1b40a0717b32') }))
+    })
+})
 
 app.post(`${baseurl}/auth`, (req,res) => {
 
     User.find({username: req.body.username}, function(err,obj) {
         if (err) return err
-        else if (obj.length == 0) {
-            res.send("Username was not found")
-        } else{
-        console.log(`Attempting to authenticate user: ${obj[0].username}`)
-        obj[0].comparePassword(req.body.password, function(err, isAuth) {
-            if (err) return(err)
-            if (isAuth) res.send("user is authenticated!")
-            if (!isAuth) res.send("user auth failed")
-        }   
-        )
-       }
+        else if (obj.length == 0) res.send("Username was not found")
+        else {
+            console.log(`Attempting to authenticate user: ${obj[0].username}`)
+            obj[0].grantToken(req.body.password, function(err, token) {
+                if (err) return(err)
+                if (!token) return(res.status(404).send("Your identity could not be verified"))
+                res.cookie("jwt", token, {secure: false, httpOnly: false})
+                res.send()
+            })
+        }
     })
 })
 
--- user.js
+++ user.js
@@ -1,5 +1,8 @@
 import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { access } from 'fs';
+import { decode } from 'punycode';
 
 var Schema = mongoose.Schema;
 
@@ -30,5 +33,61 @@ userSchema.methods.comparePassword = function(candidatePassword,next) {
 
     })
 }
+
+
+userSchema.statics.getInformation = function(token, next) {
+
+    userSchema.statics.authRun(token, function(err,obj){
+        if (err) next(err)
+        next(null, obj['user'])
+    })
+}
+
+userSchema.statics.authRun = function(token, next){
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+        if (err) next (err)
+        next (null,decoded)
+    })
+
+}
+
+userSchema.methods.grantToken = function(candidatePassword,next){
+
+    var userId = this._id
+
+    console.log("here")
+    bcrypt.compare(candidatePassword,this.password,function(err,isMatch){
+        if(err) return (err);
+        if(isMatch){
+
+            var payload = { user: userId }
+            var refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+                expiresIn: process.env.REFRESH_TOKEN_LIFE
+            })
+        
+            // that.refreshToken = refreshToken
+
+
+            var accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: process.env.ACCESS_TOKEN_LIFE
+            })
+
+            next(null,accessToken)
+    }
+    else{next(null,null)}
+    })
+
+
+
+
+}
+
+userSchema.methods.updateRefreshToken = function() {
+
+
+
+}
+
 
 export default mongoose.model("user", userSchema)
