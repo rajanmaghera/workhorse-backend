1868dd0b21083a20c22f5f3ffbbdf148a5d6a720
Rajan Maghera <rmaghera@ualberta.ca>
2021-02-18 03:29:53 -0700

    Added dummy data to GraphQL

--- app-graphql.js
+++ app-graphql.js
@@ -1,11 +1,12 @@
 import { graphql, buildSchema } from "graphql";
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
-
+import schema from './schema.js'
 const app = express();
 
 app.use("/graphql", graphqlHTTP({
-
+    schema,
+    graphiql: true
 }));
 
 app.listen(4000, () => {
--- schema.js
+++ schema.js
@@ -1,9 +1,19 @@
-import { graphql, GraphQLID, GraphQLObjectType, GraphQLString } from 'graphql'
+import { addResolveFunctionsToSchema } from 'apollo-server-express'
+import { graphql, GraphQLID, GraphQLInt, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql'
+import _ from 'lodash'
+
+// Dummy Data
+
+var books = [
+    { name: 'abc', genre: 'fantasy', id: 1 },
+    { name: 'def', genre: 'fantasy', id: 2 },
+    { name: 'ghi', genre: 'romance', id: 3 }
+]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLID },
+        id: { type: GraphQLInt },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
@@ -12,7 +22,18 @@ const BookType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-        book: { type: BookType },
-        args: { id: { type: GraphQLID } }
+        book: {
+            type: BookType,
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                return _.find(books, { id: args.id })
+            }
+        },
     }
+})
+
+export default new GraphQLSchema({
+    query: RootQuery
 })
